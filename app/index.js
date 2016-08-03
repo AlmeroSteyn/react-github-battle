28c9466a4414b4f729933d97d7b39a0aa6c624b1
AlmeroSteyn <almero.steyn@gmail.com>
2016-08-03 13:06:02 +0200

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,76 @@
+// var USER_DATA = {
+//     name: 'Tyler McGinnis',
+//     username: 'tylermcginnis',
+//     image: 'https://avatars0.githubusercontent.com/u/2933430?v=3&s=460'
+// };
+
+var React = require('react');
+var ReactDom = require('react-dom');
+var routes = require( './config/routes');
+
+// var HelloWorld = React.createClass({
+//     render: function() {
+//         return (
+//             <div>Hello {this.props.name}</div>
+//         )
+//     }
+// });
+
+// var ProfilePic = React.createClass({
+//     render: function() {
+//         return (
+//             <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+//         )
+//     }
+// });
+
+// var Link = React.createClass({
+//     changeUrl: function(){
+//         window.location.replace(this.props.href)
+//     },
+//     render: function(){
+//         return (
+//             <span style={{color: 'blue', cursor: 'pointer'}}
+//             onClick={this.changeUrl}>
+//                 {this.props.children}
+//             </span>
+//         )
+//     }
+// });
+
+// var ProfileLink = React.createClass({
+//     render: function(){
+//         return (
+//             <div>
+//                 <Link href={'https://www.github.com/' + this.props.username}>
+//                     {this.props.username}
+//                 </Link>
+//             </div>
+//         )
+//     }
+// });
+
+// var ProfileName = React.createClass({
+//     render: function(){
+//         return (
+//             <div>{this.props.name}</div>
+//         )
+//     }
+// });
+
+// var Avatar = React.createClass({
+//     render: function(){
+//         return (
+//             <div>
+//                 <ProfilePic imageUrl={this.props.user.image}/>
+//                 <ProfileName name={this.props.user.name}/>
+//                 <ProfileLink username={this.props.user.username}/>
+//             </div>
+//         )
+//     }
+// });
+
+ReactDom.render(
+    routes,
+    document.getElementById('app')
+);
